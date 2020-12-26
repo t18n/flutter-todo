3661ac3b1ada223790f224dc548303e373fe6fe7
Turbo Thinh <turbothinh@gmail.com>
2020-12-26 01:41:38 +0100

    feat: add name list generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 import 'randomWords.dart';
 
@@ -9,15 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
--- lib/randomWords.dart
+++ lib/randomWords.dart
@@ -1,5 +1,5 @@
-
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class RandomWords extends StatefulWidget {
@@ -8,9 +8,46 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asString + 'Hello2');
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Startup Name Generator'),
+    ),
+    body: _buildSuggestions(),
+  );
+
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        // For odd rows, add Divider widget to visually separate the entries
+        if (i.isOdd) return Divider();
+
+        // divides i by 2 and returns an integer result
+        // This calculates the actual number of word pairings in the ListView, minus the divider widgets
+        final index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
   }
 }
